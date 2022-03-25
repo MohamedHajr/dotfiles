1c2749736e0a9ddea7fc004e6a671535c24ce6ad
Mo Hajr <mo@hajr.io>
2022-03-25 06:34:20 +0000

    Moving git global configuration to installation script. Adding vim-go as a submodule

--- install.sh
+++ install.sh
@@ -8,6 +8,14 @@ echo $(pwd)
 # Get all upgrades
 sudo apt upgrade -y
 
+#Git Global Configuration
+git config --global core.editor /usr/bin/vim
+git config --global alias.tree "log --oneline --decorate --all --graph"
+
+##Github account info
+git config --global user.name "Mo Hajr"
+git config --global user.email "mo@hajr.io"
+
 # See our bash changes
 source ~/.bash_profile
 

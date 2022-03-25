9ae0a160424dc26e551c4506328dce2d04d5c185
Mo Hajr <mo@hajr.io>
2022-03-25 06:39:42 +0000

    cloning and updating git submodules in the installation script

--- install.sh
+++ install.sh
@@ -10,11 +10,18 @@ sudo apt upgrade -y
 
 #Git Global Configuration
 git config --global core.editor /usr/bin/vim
+
+##Git Aliases
 git config --global alias.tree "log --oneline --decorate --all --graph"
 
 ##Github account info
 git config --global user.name "Mo Hajr"
 git config --global user.email "mo@hajr.io"
+
+##installing git submodules
+git submodule init
+git submodule update
+git pull --recurse-submodules
 
 # See our bash changes
 source ~/.bash_profile

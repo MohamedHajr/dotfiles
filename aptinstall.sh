64347f2a72d90e537fb80425fa214af785e80a7e
Mo Hajr <mo@hajr.io>
2022-04-25 21:56:20 +0200

    Update aptinstall.sh

--- aptinstall.sh
+++ aptinstall.sh
@@ -33,7 +33,7 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
 
-#Nodejs
+#Nodejs - Get the latest version from Node source https://github.com/nodesource/distributions#debinstall
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
